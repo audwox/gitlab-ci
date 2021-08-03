fb8eccd00a9de524db1575f5e2782bff19434c86
audwox <audwox@gmail.com>
2021-08-03 19:27:17 +0900

    Update run-sparrow.sh

--- run-sparrow.sh
+++ run-sparrow.sh
@@ -1,13 +1,7 @@
-echo start
-echo /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
+/sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
 rc=$?
-echo in the middle, $rc
-ls -l result
 risk1=`grep -P '1 \S+ \d+' <result | awk '{s+=$3} END {print s}'`
-echo 1111
 risk2=`grep -P '2 \S+ \d+' <result | awk '{s+=$3} END {print s}'`
-echo 2222
-echo $SP_R1 == $SP_R2
 cat result
 
 [ "0$rc" -ne 0 ] && {
