89811642ca24bdb735204442ad82412289608edb
audwox <audwox@gmail.com>
2021-08-03 17:18:08 +0900

    Create run-sparrow.sh

---
+++ run-sparrow.sh
@@ -0,0 +1,12 @@
+echo start
+echo /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
+rc=$?
+echo in the middle, $rc
+ls -l result
+risk1=`grep -P '1 \S+ \d+' <result | awk '{s+=$3} END {print s}'`
+echo 1111
+risk2=`grep -P '2 \S+ \d+' <result | awk '{s+=$3} END {print s}'`
+echo 2222
+cat result
+[ "$rc" -ne 0 ] && exit 1
+[ "$risk1" -gt "$SP_R1" -o "$risk2" -gt "$SP_R2" ] && { echo "Error: Too many risks."; exit 2 }
