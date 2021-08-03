2c50b32e168be2a92eed597272dfdccea32759d4
audwox <audwox@gmail.com>
2021-08-03 20:20:58 +0900

    Update run-sparrow.sh

--- run-sparrow.sh
+++ run-sparrow.sh
@@ -1,7 +1,5 @@
 /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
 rc=$?
-grep -P '1\s+\S+\s+\d+' result
-grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'
 risk1=`grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
 risk2=`grep -P '2\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
 cat result
@@ -11,7 +9,7 @@ cat result
 	exit 1
 }
 [ "0$risk1" -gt "0$SP_R1" -o "0$risk2" -gt "0$SP_R2" ] && {
-	echo "Error: Too many risks."
+	echo "Error: Too many high risk issues: $((0$risk1 + 0$risk2))"
 	exit 2
 }
 exit 0
