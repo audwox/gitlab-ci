317142b4e0430d90f5195ca9019ca6d8dbbb04fe
audwox <audwox@gmail.com>
2021-08-03 20:04:02 +0900

    Update run-sparrow.sh

--- run-sparrow.sh
+++ run-sparrow.sh
@@ -1,9 +1,9 @@
 /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
 rc=$?
-grep -P '1 \S+ \d+' result
-grep -P '1 \S+ \d+' result | awk '{s+=$3} END {print s}'
-risk1=`grep -P '1 \S+ \d+' result | awk '{s+=$3} END {print s}'`
-risk2=`grep -P '2 \S+ \d+' result | awk '{s+=$3} END {print s}'`
+grep -P '1\s+\S+\s+\d+' result
+grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'
+risk1=`grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
+risk2=`grep -P '2\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
 cat result
 
 [ "0$rc" -ne 0 ] && {
