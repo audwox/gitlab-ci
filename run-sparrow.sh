563fb059e5f326e3ae43881df15c1d00d34e3224
audwox <audwox@gmail.com>
2021-08-04 15:27:35 +0900

    Update run-sparrow.sh

--- run-sparrow.sh
+++ run-sparrow.sh
@@ -1,5 +1,4 @@
-cat pass.txt | /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -S $SP_HOST -SD $BASE_DIR > result
-#cat pass.txt | /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
+/sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
 rc=$?
 risk1=`grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
 risk2=`grep -P '2\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
