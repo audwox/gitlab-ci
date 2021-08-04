5c654d34f639a67b6bcfbff3869f843eda680134
audwox <audwox@gmail.com>
2021-08-04 17:50:19 +0900

    Update run-sparrow.sh

--- run-sparrow.sh
+++ run-sparrow.sh
@@ -1,3 +1,10 @@
+# sparrow-client.sh 의 출력에서 risk 단계가 1, 2인 이슈(고위험 이슈)의 수를 비교하고
+# 고위험 이슈가 많으면 실행 종료 시 exit code로 2를 리턴.
+# exit codes:
+# 0 - success
+# 1 - something wrong
+# 2 - too many issues
+
 /sparrow5_client/sparrow-client.sh -P JUST -U "$SP_ID" -PW ./pass.txt -S $SP_HOST -SD $BASE_DIR > result
 rc=$?
 risk1=`grep -P '1\s+\S+\s+\d+' result | awk '{s+=$3} END {print s}'`
